5dfaf5eb86476301873596e92b8a6eb2a1648134
Eric <e.sy@oceans.ubc.ca>
2017-03-29 17:14:08 -0700

    data type changed from smallint to int

--- table_geo.sql
+++ table_geo.sql
@@ -388,7 +388,7 @@ CREATE TABLE geo.ices_area(
 CREATE TABLE geo.simple_area_cell_assignment_raw(
   id SERIAL PRIMARY KEY,
   marine_layer_id smallint NULL,
-  area_id smallint NULL,
+  area_id int NULL,
   fao_area_id smallint NULL,
   cell_id float NULL,
   water_area float NULL,
